3ebe555b3c43ae41630b9553306d66787951eace
bdurushia <b.durushia@gmail.com>
2021-11-07 19:27:14 -0600

    added main HTML and styling

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+// var hide = function(elements) {
+//     var elements = document.querySelector(".hide");
+//     for (var i = 0; i < elements.length; i++) {
+//         elements[i].style.display = "none";
+//     }
+// };
+
